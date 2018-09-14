3bd1e9daa38979197be8060ed7515439a0c958ae
BaneleMK <banelen7x@gmail.com>
2018-09-15 00:11:18 +0200

    Just fixing up my Git repo

--- comp.sh
+++
@@ -1,4 +0,0 @@
-#gcc libft/libft.a -o "test $1" -I ~/.brew/include -L ~/.brew/lib -lSDL2 $1 
-gcc -Wall -Werror -Wextra main.c raycasting.c controls.c -o wolf3d `pkg-config --cflags --libs sdl2` -lm libft/libft.a
-./wolf3d test.wf3d
-echo without a hitch sir
